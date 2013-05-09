9778c35e6e88b9d58e39b3643f017f006a8fec22
Mateusz Koncikowski <mkocikowski@gmail.com>
2013-05-09 13:32:02 +0200

    fabfile modification

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,5 @@ from fabric.api import *
 
 def clean_database():
     print "Clearing database"
-    mysql = "/Applications/XAMPP/xamppfiles/bin/mysql"
-    database_scheme = "src/main/resources/forum/db.sql"
-    local("%s -uroot forum < %s" % (mysql, database_scheme))
-    print "Clearing database finished"
+    local("/Applications/XAMPP/xamppfiles/bin/mysql -uroot forum < src/main/resources/forum/db.sql")
+    print "Clearing database has been finished"
