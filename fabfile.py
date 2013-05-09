b1f4099ca0db61d5d54ca7309a4a7e64788411d9
Mateusz Koncikowski <mkocikowski@gmail.com>
2013-05-09 14:22:03 +0200

    simplifying mysql execution

--- fabfile.py
+++ fabfile.py
@@ -5,5 +5,5 @@ from fabric.api import *
 
 def clean_database():
     print "Clearing database"
-    local("/Applications/XAMPP/xamppfiles/bin/mysql -uroot forum < src/main/resources/forum/db.sql")
+    local("mysql -uroot forum < src/main/resources/forum/db.sql")
     print "Clearing database has been finished"
