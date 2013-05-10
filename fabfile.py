95427bc00809c61121e2568fe98ac0661372d4b1
Mateusz Koncikowski <mkocikowski@gmail.com>
2013-05-10 08:40:37 +0200

    adding deploy job

--- fabfile.py
+++ fabfile.py
@@ -7,3 +7,9 @@ def clean_database():
     print "Clearing database"
     local("mysql -uroot forum < src/main/resources/forum/db.sql")
     print "Clearing database has been finished"
+
+
+def deploy():
+    print "Deploying application on the server"
+    local("cp src/main/resources/forum/* /Applications/XAMPP/htdocs/forum/")
+    print "Deploying application has been finished"
