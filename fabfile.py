bce095ea893933bcb3090d96504a6dc9b246e302
Mateusz Koncikowski <mkocikowski@gmail.com>
2013-05-10 09:33:33 +0200

    adding new commands to fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,13 +3,19 @@ __author__ = 'mateusz'
 from fabric.api import *
 
 
+def clean_workspace():
+    print "Clearing workspace"
+    local("rm -rf *")
+    print "Clearing workspace has been finished"
+
+
 def clean_database():
     print "Clearing database"
     local("mysql -uroot forum < src/main/resources/forum/db.sql")
     print "Clearing database has been finished"
 
 
-def deploy():
-    print "Deploying application on the server"
-    local("cp src/main/resources/forum/* /Applications/XAMPP/htdocs/forum/")
-    print "Deploying application has been finished"
+def build_release_package():
+    print "Building release package"
+    local("zip -rj release_package.zip  src/main/resources/*")
+    print "Building release package has been finished"
